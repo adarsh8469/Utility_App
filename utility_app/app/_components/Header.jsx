a5541a81605cfb12661d504852d3e0a3d214587a
adarsh8469 <apdon9956@gmail.com>
2025-05-09 17:25:51 +0530

    navbar done

--- utility_app/app/_components/Header.jsx
+++ utility_app/app/_components/Header.jsx
@@ -1,17 +1,25 @@
 import Image from 'next/image'
 import React from 'react'
+import { Button } from '@/components/ui/button'
 
 function Header() {
     return (
-        <div className='p-5 shadow-sm flex'>
+        <div className='p-5 shadow-sm flex justify-between'>
             <div className='flex items-center gap-8'>
                 <Image src='./logo.svg' alt='Intelli Grid'
                     width={190} height={110} />
-                <div className='flex items-center gap-6'>
-                    <h2>Home</h2>
-                    <h2>Services</h2>
-                    <h2>About us</h2>
+                <div className='md:flex items-center gap-6 cursor-pointer hidden'>
+                    <h2 className='hover:scale-105  font-semibold transition duration-200'>Home</h2>
+                    <h2 className='hover:scale-105  font-semibold transition duration-200'>Services</h2>
+                    <h2 className='hover:scale-105  font-semibold transition duration-200'>Explore</h2>
+                    <h2 className='hover:scale-105 text-[#e42b50f1] font-semibold transition duration-200'>Subscription</h2>
+                    <h2 className='hover:scale-105  font-semibold transition duration-200'>About us</h2>
                 </div>
+            </div>
+            <div>
+                <Button className="!bg-[#e42b50f1] hover:!bg-[#e42b50f1] text-white font-semibold">
+                    Get Started
+                </Button>
             </div>
         </div>
     )
--- utility_app/app/page.js
+++ utility_app/app/page.js
@@ -1,11 +1,10 @@
 import Image from "next/image";
 import { Button } from "@/components/ui/button";
-import Header from "./_components/Header"
 
 export default function Home() {
   return (
     <div>
-      <Header/>
+      
     </div>
   );
 }
