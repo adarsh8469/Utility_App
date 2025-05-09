1bc7fe7145c7110de1ce99d4f047c54de296f6a8
adarsh8469 <apdon9956@gmail.com>
2025-05-09 18:09:28 +0530

    middle page minute chages

--- utility_app/app/_components/Hero.jsx
+++ utility_app/app/_components/Hero.jsx
@@ -5,7 +5,7 @@ import { Search } from 'lucide-react'
 function Hero() {
   return (
     <div className='flex items-center flex-col justify-center pt-14 pb-7 gap-3'>
-        <h1 className='font-bold text-[30px] mb-10 cursor-pointer'>Welcome to <span className='text-[#e42b50f1] cursor-pointer'><b>Intelli Grip</b></span></h1>
+        <h1 className='font-bold text-[30px] mb-4 cursor-pointer'>Welcome to <span className='text-[#e42b50f1] cursor-pointer'><b>Intelli Grip</b></span></h1>
         <h2 className='font-bold text-[56px] text-center cursor-pointer'>Find Home <span className='text-[#e42b50f1] '><b>Sevice/Repair</b></span> Near You</h2>
         <h2 className='text-[23px] text-gray-700 cursor-pointer'>Explore Best Home Service & Repair Near You</h2>
         <div className='mt-5 flex gap-5 items-center cursor-pointer'>
